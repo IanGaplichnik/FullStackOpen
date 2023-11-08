a67e8e94452af8b11ea98ae074f2feec9ae14c99
IanGaplichnik <ian.gaplichnik@protonmail.ch>
2023-11-08 13:19:15 +0200

    Exercises 2.4 and 2.5 course info complete
    - List of courses is now dynamic

--- part2/CourseInfo/src/components/Course.jsx
+++ part2/CourseInfo/src/components/Course.jsx
@@ -1,8 +1,4 @@
-const Part = ({ part }) => {
-  return (
-    <p>{part.name} {part.exercises} </p>
-  )
-}
+const Part = ({ part }) => <p>{part.name} {part.exercises} </p>
 
 const Content = ({ parts }) => {
   const total = parts.reduce((s, p) => s + p.exercises, 0)
@@ -10,16 +6,15 @@ const Content = ({ parts }) => {
   return (
     <div>
       {parts.map(part => <Part key={part.id} part={part}> </Part>)}
-      <p>total of {total} exercises</p>
+      <b>total of {total} exercises</b>
     </div>
   )
 }
 
 const Course = ({ course }) => {
-
   return (
     <div>
-      <h1>{course.name}</h1>
+      <h2>{course.name}</h2>
       <Content parts={course.parts}></Content>
     </div>
   )
