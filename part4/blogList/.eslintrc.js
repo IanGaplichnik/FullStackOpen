1a02852803d221e95fad698a667d88e721524564
IanGaplichnik <ian.gaplichnik@protonmail.ch>
2023-11-23 13:47:33 +0200

    Exercises 4.2-4.3 blogslist complete
    - Improved file structure
    - Basic testing setup

--- part4/blogList/.eslintrc.js
+++ part4/blogList/.eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   'env': {
-    'browser': true,
+    'node': true,
     'commonjs': true,
-    'es2021': true
+    'es2021': true,
+    'jest': true
   },
   'extends': 'eslint:recommended',
   'overrides': [
@@ -37,6 +38,14 @@ module.exports = {
     'semi': [
       'error',
       'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
     ]
   }
 }
