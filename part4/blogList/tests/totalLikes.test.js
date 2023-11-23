767dc5907de1fa1a935914159e5ae15bd3f7a15e
IanGaplichnik <ian.gaplichnik@protonmail.ch>
2023-11-23 21:45:35 +0200

    Exercises 4.4-4.7 complete
    - Implemented functions totalLikes, favoriteBlog, mostBlogs, mostLikes
    - Implemented tests for functions above

---
+++ part4/blogList/tests/totalLikes.test.js
@@ -0,0 +1,74 @@
+const totalLikes = require('../utils/list_helper').totalLikes
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithZeroAndMultipleLikes = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    }
+  ]
+
+  const listWithMultipleAndMultipleLikes = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = totalLikes([])
+    expect(result).toBe(0)
+  })
+
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('positive and zero likes', () => {
+    const result = totalLikes(listWithZeroAndMultipleLikes)
+    expect(result).toBe(7)
+  })
+
+  test('positive and positive likes', () => {
+    const result = totalLikes(listWithMultipleAndMultipleLikes)
+    expect(result).toBe(10)
+  })
+
+})
--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,7 +1,48 @@
-const dummy = (blogs) => {
-  return 1
+const _ = require('lodash')
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => sum + blog.likes
+
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? null
+    : _.maxBy(blogs, (blog) => blog.likes)
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0)
+    return null
+
+  const authorHasBlogs = _.countBy(blogs, 'author')
+  const authorWithMostBlogs = _.maxBy(Object.keys(authorHasBlogs), (author) => authorHasBlogs[author])
+  return {
+    author: authorWithMostBlogs,
+    blogs: authorHasBlogs[authorWithMostBlogs]
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0)
+    return null
+
+  const authorBlogs = _.groupBy(blogs, 'author')
+  const authorLikesDict = _.mapValues(authorBlogs,
+    function calculateLikesPerAuthor(author) {
+      return author.reduce((sum, blog) => sum + blog.likes, 0)
+    })
+  const authorMostLikes = _.maxBy(Object.keys(authorLikesDict), (author) => authorLikesDict[author])
+  return {
+    author: authorMostLikes,
+    likes: authorLikesDict[authorMostLikes]
+  }
 }
 
 module.exports = {
-  dummy
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
