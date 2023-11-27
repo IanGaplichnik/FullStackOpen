e45d9d0d778360faaf671e9836d5c4c5aaee882b
IanGaplichnik <ian.gaplichnik@protonmail.ch>
2023-11-27 16:11:33 +0200

    Exercise 4.8 bloglist complete
    - Prepared test infrastructure: created new cloud database,
      functionality beforeEach and afterAll tests
    - Added first test case testing GET request
    - Changed GET request router to use async/await syntax instead of
      promises

---
+++ part4/blogList/tests/bloglist_api.test.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const initialBlogList = require('./test_helper').initialBlogList
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  console.log('cleared')
+
+  const blogObjects = initialBlogList.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+
+test('server returns right amount of blogs', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body).toHaveLength(initialBlogList.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
