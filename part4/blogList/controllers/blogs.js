f0fcfdab420bcd0ad7dc79e89846d898844c6e08
IanGaplichnik <ian.gaplichnik@protonmail.ch>
2023-11-28 12:53:49 +0200

    Exercise 4.12 complete
    - Added functionality that return status 400, if title or url fields are
      missing from request
    - Added corresponding tests

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -1,22 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs)
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({})
+    response.json(blogs)
+  } catch(exception) {
+    next(exception)
+  }
 })
 
-blogsRouter.post('/', async (request, response) => {
-  console.log('IN POST')
+blogsRouter.post('/', async (request, response, next) => {
+  if (!Object.hasOwn(request.body, 'title')
+    || !Object.hasOwn(request.body, 'url'))
+    return response.status(400).end()
+
   const blogRequest = {
     likes: request.body.likes ? request.body.likes : 0,
     ...request.body
   }
-  console.log(blogRequest)
   const blog = new Blog(blogRequest)
 
-  const result = await blog.save()
-  response.status(201).json(result)
+  try {
+    const result = await blog.save()
+    response.status(201).json(result)
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter
--- part4/blogList/tests/bloglist_api.test.js
+++ part4/blogList/tests/bloglist_api.test.js
@@ -15,60 +15,85 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
+describe('when there is initially some saved blogs', () => {
+  test('server returns right amount of blogs', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-test('server returns right amount of blogs', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    expect(response.body).toHaveLength(initialBlogList.length)
+  })
 
-  expect(response.body).toHaveLength(initialBlogList.length)
-})
+  test('backend renames __id parameter to id', async () => {
+    const response = await api.get('/api/blogs')
 
-test('backend renames __id parameter to id', async () => {
-  const response = await api.get('/api/blogs')
+    response.body.map(blog => expect(blog.id).toBeDefined())
+  }, 100000)
 
-  response.body.map(blog => expect(blog.id).toBeDefined())
-}, 100000)
+  test('post request saves blog correctly', async () => {
+    const exampleBlog = {
+      title: 'Template Title',
+      author: 'Template author',
+      url: 'Template URL',
+      likes: 20
+    }
 
-test('post request saves blog correctly', async () => {
-  const exampleBlog = {
-    title: 'Template Title',
-    author: 'Template author',
-    url: 'Template URL',
-    likes: 20
-  }
+    await api
+      .post('/api/blogs')
+      .send(exampleBlog)
 
-  await api
-    .post('/api/blogs')
-    .send(exampleBlog)
+    const response = await api.get('/api/blogs/')
+    const lastBlogIndex = response.body.length - 1
+    const receivedBlog = response.body[lastBlogIndex]
+    delete receivedBlog.id
 
-  const response = await api.get('/api/blogs/')
-  const lastBlogIndex = response.body.length - 1
-  const receivedBlog = response.body[lastBlogIndex]
-  delete receivedBlog.id
+    expect(response.body.length).toEqual(initialBlogList.length + 1)
+    expect(exampleBlog).toStrictEqual(receivedBlog)
+  }, 100000)
 
-  expect(response.body.length).toEqual(initialBlogList.length + 1)
-  expect(exampleBlog).toStrictEqual(receivedBlog)
-})
+  test('post request without likes saves 0 likes', async () => {
+    const blogWOLikes = {
+      title: 'Template Title',
+      author: 'Template author',
+      url: 'Template URL',
+    }
 
-test('post request without likes saves 0 likes', async () => {
-  const blogWOLikes = {
-    title: 'Template Title',
-    author: 'Template author',
-    url: 'Template URL',
-  }
+    await api
+      .post('/api/blogs')
+      .send(blogWOLikes)
 
-  await api
-    .post('/api/blogs')
-    .send(blogWOLikes)
+    const response = await api.get('/api/blogs/')
+    const lastBlogIndex = response.body.length - 1
+    const receivedBlog = response.body[lastBlogIndex]
 
-  const response = await api.get('/api/blogs/')
-  const lastBlogIndex = response.body.length - 1
-  const receivedBlog = response.body[lastBlogIndex]
+    expect(blogWOLikes.likes).toBeUndefined()
+    expect(receivedBlog.likes).toEqual(0)
+  })
 
-  expect(blogWOLikes.likes).toBeUndefined()
-  expect(receivedBlog.likes).toEqual(0)
+  test('post request without title returns 400', async () => {
+    const blogWOTitle = {
+      author: 'Template author',
+      url: 'Template URL',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogWOTitle)
+      .expect(400)
+  })
+
+  test('post request without url returns 400', async () => {
+    const blogWOUrl = {
+      title: 'Template Title',
+      author: 'Template author',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogWOUrl)
+      .expect(400)
+  })
 })
 
 afterAll(async () => {
